3d337e079f552ad6f37026c9aa1930a0665d71ad
gabby-m056 <144246046+gabby-m056@users.noreply.github.com>
2025-10-16 16:37:58 +0100

    Edit1

--- Assets/FindCar.cs
+++ Assets/FindCar.cs
@@ -11,6 +11,7 @@ public class FindCar : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        GameObject target = GameObject.Find("Car");
+        gameObject.transform.LookAt(target.transform);
     }
 }
